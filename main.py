d152e781ebdc8ce6e4ff3b5428efc2073033ec94
Manoel Britto <manoelbritto@gmail.com>
2023-02-12 21:42:13 -0500

    commit

--- main.py
+++ main.py
@@ -16,32 +16,39 @@ def rate_guess (number):
     """
     if number == initial_game_number:
         return "win"
-    if abs(number-initial_game_number) > 5:
-        return "High"
-    else:
+    if initial_game_number > number:
         return "Low"
+    else:
+        return "High"
+
+def type_of_game (level):
+    """define the number of changes the player can have"""
+    if level == "easy":
+        repeat = 10
+    else:
+        repeat = 5
+
+    for chances in range(0, repeat):
+        number_player = input(f"You have {chances+1} of {repeat}\nType your number: ")
+        result_game = rate_guess(int(number_player))
+        if result_game == "win":
+            break
+        print(result_game)
+    return result_game
 
 #game play
 while game_repeat:
     print ("This game is for you to guess a number between 0 to 100")
     game_level = input("To play, type which level you want Easy or Hard? ").lower()
 
-    if game_level == 'easy':
-        for _ in range(0,10):
-            number_player = input("Type your number: ")
-            result_game = rate_guess(int(number_player))
-            if result_game == "win":
-                break
-            print(result_game)
+    if type_of_game (game_level) == "win":
+        print("Congrats you win")
+    else:
+        print("Sorry you lose")
 
-        if result_game == "win":
-            print("Congrats you win")
-        else:
-            print("Sorry you lose")
+    game_ask = input("Would you like to continue yes or no? ").lower()
+    if game_ask == 'yes':
+        game_repeat = True
 
-        game_ask = input("Would you like to continue yes or no? ").lower()
-        if game_ask == 'yes':
-            game_repeat = True
-
-        else:
-            game_repeat = False
+    else:
+        game_repeat = False
